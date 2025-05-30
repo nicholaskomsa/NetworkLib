1ea86c9b04dc2877ad36a8110015e743ad1cef13
nicholaskomsa <nicholaskomsa@hotmail.com>
2025-05-30 07:53:40 -0400

    changed to WinMain

--- Animator/main.cpp
+++ Animator/main.cpp
@@ -1,6 +1,6 @@
 #include <SDL3/SDL.h>
 
-#define WINDOWS_LEAN_AND_MEAN
+#define WIN32_LEAN_AND_MEAN
 #define NOMINMAX
 #include <windows.h>
 
@@ -10,25 +10,21 @@
 #include <numeric>
 #include <random>
 #include <algorithm>
+#include <execution>
 
-#include <Algorithms.h>
 #include <FloatSpaceConvert.h>
 #include <future>
 
-int main() {
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nShowCmd){
 
-    auto [width, height] = FloatSpaceConvert::getDimensions(100000);
-
+    auto [width, height] = FloatSpaceConvert::getDimensions(50000);
 
     SDL_GLContext glContext;
     GLuint texture;
     SDL_Window* window;
+
     std::vector<std::uint32_t> pixels(width * height);
-
     std::vector<float> floats(pixels.size());
-
-    std::mt19937 random;
-    std::uniform_real_distribution<float> range(-1.0f, 1.0f);
 
     auto initOpenGL = [&]() {
         // Initialize SDL3
@@ -40,7 +36,8 @@ int main() {
         // Create a fullscreen window
         window = SDL_CreateWindow("SDL3 Fullscreen Example",
             1920, 1080
-            , SDL_WINDOW_BORDERLESS | SDL_WINDOW_OPENGL | SDL_WINDOW_FULLSCREEN); //SDL_WINDOW_FULLSCREEN || 
+            , SDL_WINDOW_MOUSE_FOCUS | SDL_WINDOW_INPUT_FOCUS | SDL_WINDOW_BORDERLESS 
+            | SDL_WINDOW_OPENGL | SDL_WINDOW_FULLSCREEN );
 
         if (!window) {
             SDL_Log("Window could not be created! SDL_Error: %s\n", SDL_GetError());
@@ -50,7 +47,7 @@ int main() {
 
         glContext = SDL_GL_CreateContext(window);
 
-        SDL_GL_SetSwapInterval(0); //triple buffering
+        SDL_GL_SetSwapInterval(0); 
 
         glGenTextures(1, &texture);
         glBindTexture(GL_TEXTURE_2D, texture);
@@ -77,13 +74,18 @@ int main() {
         glTexCoord2f(1, 1); glVertex2f(1, 1);
         glTexCoord2f(0, 1); glVertex2f(-1, 1);
         glEnd();
-
+  
+        glFlush();
+  
         SDL_GL_SwapWindow(window);
         };
 
     bool running = true;
 
     auto doEvents = [&]() {
+        
+        std::this_thread::yield();
+
         SDL_Event event;
         while (SDL_PollEvent(&event)) {
             if (event.type == SDL_EVENT_QUIT) {
@@ -91,10 +93,13 @@ int main() {
             }
         }
         };
+    
+    std::mt19937 random;
+    std::uniform_real_distribution<float> range(-1.0f, 1.0f);
 
     auto step = [&]() {
 
-        std::generate(floats.begin(), floats.end(), [&]() {
+        std::generate(std::execution::seq, floats.begin(), floats.end(), [&]() {
             return range(random);
             });
 
@@ -105,53 +110,36 @@ int main() {
     initOpenGL();
 
     std::size_t generation = 0;
-    std::chrono::milliseconds mLengthOfStep{ 1000 / 10 };
-
     using namespace std::chrono;
     using namespace std::chrono_literals;
 
-    nanoseconds lag(0), timeElapsed(0s);
+    constexpr std::chrono::milliseconds mLengthOfStep =  milliseconds(1s) / 10;
+
+    nanoseconds lag(0), elapsedTime(0s);
     steady_clock::time_point nowTime, fpsStartTime
         , oldTime = steady_clock::now(), fpsEndTime = oldTime;
 
-    std::uint32_t frameCount = 0, tickCount = 0;
+    std::uint32_t tickCount = 0;
 
     do {
         nowTime = high_resolution_clock::now();
-        timeElapsed = duration_cast<nanoseconds>(nowTime - oldTime);
+        elapsedTime = duration_cast<nanoseconds>(nowTime - oldTime);
         oldTime = nowTime;
 
-        lag += timeElapsed;
+        lag += elapsedTime;
         while (lag >= mLengthOfStep) {
 
             step();
 
+            render();
+
             lag -= mLengthOfStep;
 
             ++tickCount;
-
-            render();
-            ++frameCount;
-
-            fpsStartTime = high_resolution_clock::now();
-
-            nanoseconds fpsElapsedTime(duration_cast<nanoseconds>(fpsStartTime - fpsEndTime));
-            if (fpsElapsedTime >= 1s) {
-
-                fpsEndTime = fpsStartTime;
-
-   
-              // std::cout << std::format("Ticks: {} FPS: {}", tickCount, frameCount);
-                   
-
-                ++generation;
-
-                frameCount = 0;
-                tickCount = 0;
-            }
         }
 
         doEvents();
+
 
     } while (running && generation < 100 );
 
