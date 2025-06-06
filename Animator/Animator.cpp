447b2984d131897ea387079338bea7ff2c421bb1
nicholaskomsa <nicholaskomsa@hotmail.com>
2025-06-06 12:09:23 -0400

    animator seems feature complete

---
+++ Animator/Animator.cpp
@@ -0,0 +1,404 @@
+#include "Animator.h"
+
+#include <format>
+#include <vector>
+#include <numeric>
+#include <random>
+#include <algorithm>
+#include <execution>
+
+Animator::Error::Error(std::errc code, const std::string& message)
+    : std::system_error(int(code), std::generic_category(), message) {}
+
+void Animator::Error::sdlError() {
+    throw Error(std::errc::operation_canceled, std::format("SDL Error: {}", SDL_GetError()));
+}
+void Animator::Error::glewError(auto error) {
+
+    throw Error(std::errc::operation_canceled, std::format("GLew Error: {}", reinterpret_cast<const char*>(glewGetErrorString(error))));
+}
+void Animator::Error::glCompilationError(auto shaderProgram) {
+    char infoLog[512];
+    glGetProgramInfoLog(shaderProgram, 512, nullptr, infoLog);
+    throw Error(std::errc::operation_canceled, std::format("GL Shader Compilation Error: {}", infoLog));
+}
+void Animator::Error::msgbox() const {
+    MessageBoxA(nullptr, what(), "Animator Error", MB_OK | MB_ICONERROR);
+}
+   
+void Animator::render() {
+
+    glBindTexture(GL_TEXTURE_2D, mTexture);
+    glTexSubImage2D(GL_TEXTURE_2D, 0, 0, 0, mTextureWidth, mTextureHeight, GL_RGBA, GL_UNSIGNED_INT_8_8_8_8_REV, mPixels.data());
+    
+    //scale and translate == sat
+    auto sat = [&](float f, float t) {
+        return (f + t) * mScale;
+		};
+    auto satx = [&](float x) {
+		return sat(x, mX);
+	    };
+    auto saty = [&](float y) {
+        return sat(y, mY);
+        };
+
+    GLfloat quadVertices[] = {
+        // X, Y positions   // Texture Coords
+        satx(-1.0f), saty(1.0f),    0.0f, 0.0f,  // Top-left
+        satx(1.0f), saty(1.0f),     1.0f, 0.0f,  // Top-right
+        satx(-1.0f), saty(-1.0f),   0.0f, 1.0f,  // Bottom-left
+        satx(1.0f), saty(-1.0f),    1.0f, 1.0f   // Bottom-right
+    };
+
+    glBindVertexArray(mVao);
+    glBindBuffer(GL_ARRAY_BUFFER, mVbo);
+    glBufferSubData(GL_ARRAY_BUFFER, 0, sizeof(quadVertices), quadVertices);
+    glUseProgram(mShaderProgram);
+
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    glDrawArrays(GL_TRIANGLE_STRIP, 0, 4);
+    SDL_GL_SwapWindow(mWindow);
+}
+void Animator::doEvents() {
+
+    std::this_thread::yield();
+
+    auto keyRepeatGuard = [&](const auto& key, auto&& keyGuardCode) {
+        //do keys at most sometimes
+        constexpr auto keyRepeatTime = milliseconds(1s) / 3;
+        auto nowTime = steady_clock::now();
+        static auto endTime = nowTime - keyRepeatTime;
+        auto elapsed = nowTime - endTime;
+        static auto oldKey = SDLK_UNKNOWN;
+
+        if (oldKey == key && elapsed < keyRepeatTime) return;
+
+        oldKey = key;
+        endTime = nowTime;
+
+        keyGuardCode();
+
+        };
+
+
+
+    SDL_Event event;
+    while (SDL_PollEvent(&event)) {
+
+        const auto& type = event.type;
+        const auto& key = event.key.key;
+
+        auto doQuit = [&]() {
+            return type == SDL_EVENT_QUIT
+                || type == SDL_EVENT_KEY_DOWN && key == SDLK_ESCAPE;
+            };
+
+        if (doQuit()) {
+
+            mRunning = false;
+            return;
+        
+        } else if (type == SDL_EVENT_KEY_DOWN) {
+
+            keyRepeatGuard(key, [&]() {
+
+                switch (key) {
+                case SDLK_1: mColorizeMode = ColorizeMode::NICKRGB; break;
+                case SDLK_2: mColorizeMode = ColorizeMode::SHORTNRGB; break;
+                case SDLK_3: mColorizeMode = ColorizeMode::ROYGBIV; break;
+                case SDLK_4: mColorizeMode = ColorizeMode::GREYSCALE; break;
+                case SDLK_5: mColorizeMode = ColorizeMode::BINARY; break;
+
+                case SDLK_Q:
+                    if (mSelectedStripes == mStripes.begin())
+                        mSelectedStripes = mStripes.end() - 1;
+                    else
+                        mSelectedStripes = std::prev(mSelectedStripes, 1);
+                    break;
+                case SDLK_W:
+                    std::advance(mSelectedStripes, 1);
+                    if (mSelectedStripes == mStripes.end())
+                        mSelectedStripes = mStripes.begin();
+                    break;
+
+                case SDLK_LEFT:
+                    mX += mTranslateSpeed;
+                    break;
+                case SDLK_RIGHT:
+                    mX -= mTranslateSpeed;
+                    break;
+                case SDLK_UP:
+					mY -= mTranslateSpeed;
+					break;
+                case SDLK_DOWN: 
+                    mY += mTranslateSpeed;
+                    break;
+                case SDLK_A:
+                    mScale /= 2.0f;
+                    break;
+                case SDLK_S:
+                    mScale *= 2.0f;
+                    break;
+                case SDLK_R:
+					mX = 0.0f;
+					mY = 0.0f;
+					mScale = 1.0f;
+					break;
+                }
+
+                mScale = std::clamp(mScale, 0.001f, 10.0f);
+
+                });
+        }
+    }
+}
+Animator::Animator(std::size_t width, std::size_t height) {
+    mTextureWidth = width;
+    mTextureHeight = height;
+}
+Animator::~Animator() {
+    shutdown();
+}
+void Animator::setup(FloatsView floats) {
+
+    mFloats = floats;
+
+    mPixels.resize(mTextureWidth * mTextureHeight, 0.0f);
+
+    auto initGL = [&]() {
+        // Initialize SDL3
+        if (SDL_Init(SDL_INIT_VIDEO) < 0)
+            Error::sdlError();
+
+        // Create a fullscreen window
+        mWindow = SDL_CreateWindow("Float Space Animator",
+            mWindowWidth, mWindowHeight
+            , SDL_WINDOW_MOUSE_FOCUS | SDL_WINDOW_INPUT_FOCUS
+            | SDL_WINDOW_OPENGL | SDL_WINDOW_FULLSCREEN);
+
+        if (!mWindow)
+            Error::sdlError();
+
+        mGLContext = SDL_GL_CreateContext(mWindow);
+        SDL_GL_SetSwapInterval(1);
+
+        auto setupModernGL = [&]() {
+
+            auto compileShader = [&](GLenum shaderType, const std::string& source) {
+                GLuint shader = glCreateShader(shaderType);
+                const char* src = source.c_str();
+                glShaderSource(shader, 1, &src, nullptr);
+                glCompileShader(shader);
+
+                // Check for compilation errors
+                GLint success;
+                glGetShaderiv(shader, GL_COMPILE_STATUS, &success);
+                if (!success)
+                    Error::glCompilationError(shader);
+
+                return shader;
+                };
+
+            auto createShaderProgram = [&](const std::string& vertexSrc, const std::string& fragmentSrc) {
+
+                GLuint vertexShader = compileShader(GL_VERTEX_SHADER, vertexSrc);
+                GLuint fragmentShader = compileShader(GL_FRAGMENT_SHADER, fragmentSrc);
+
+                GLuint shaderProgram = glCreateProgram();
+                glAttachShader(shaderProgram, vertexShader);
+                glAttachShader(shaderProgram, fragmentShader);
+                glLinkProgram(shaderProgram);
+
+                // Cleanup shaders (they are now linked)
+                glDeleteShader(vertexShader);
+                glDeleteShader(fragmentShader);
+
+                // Check for linking errors
+                GLint success;
+                glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+
+                if (!success)
+                    Error::glCompilationError(shaderProgram);
+
+                return shaderProgram;
+                };
+
+            auto getOrtho = [&](float left = -1, float right = 1, float top = 1, float bottom = -1, float n = -1.0f, float f = 1.0f) ->std::vector<float> {
+                return {
+                    2.0f / (right - left),  0.0f,                   0.0f,                 0.0f,
+                    0.0f,                   2.0f / (top - bottom),  0.0f,                 0.0f,
+                    0.0f,                   0.0f,                   -2.0f / (f - n), 0.0f,
+                    -(right + left) / (right - left), -(top + bottom) / (top - bottom), -(f + n) / (f - n), 1.0f
+                };
+                };
+
+            glewExperimental = GL_TRUE;
+            GLenum err = glewInit();
+            if (err != GLEW_OK)
+                Error::glewError(err);
+
+            std::string vertexShader = R"(
+                #version 330 core
+                layout(location = 0) in vec2 position;
+                layout(location = 1) in vec2 texCoord;
+
+                out vec2 fragTexCoord;
+
+                uniform mat4 projection;
+
+                void main() {
+                    fragTexCoord = texCoord;
+                    gl_Position = projection * vec4(position, 0.0, 1.0);
+                }
+
+                )"
+                , fragmentShader = R"(
+                #version 330 core
+                in vec2 fragTexCoord;
+                out vec4 color;
+
+                uniform sampler2D textureSampler;
+
+                void main() {
+                    color = texture(textureSampler, fragTexCoord);
+                }
+                )";
+
+            mShaderProgram = createShaderProgram(vertexShader, fragmentShader);
+            glUseProgram(mShaderProgram);
+
+            GLint projLoc = glGetUniformLocation(mShaderProgram, "projection");
+            glUniformMatrix4fv(projLoc, 1, GL_FALSE, getOrtho().data());
+            };
+
+        auto createQuad = [&]() {
+
+            GLfloat quadVertices[] = {
+                // X, Y positions   // Texture Coords
+                -1.0f,  1.0f,       0.0f, 0.0f,  // Top-left
+                 1.0f,  1.0f,       1.0f, 0.0f,  // Top-right
+                -1.0f, -1.0f,       0.0f, 1.0f,  // Bottom-left
+                 1.0f, -1.0f,       1.0f, 1.0f   // Bottom-right
+            };
+
+            glGenVertexArrays(1, &mVao);
+            glGenBuffers(1, &mVbo);
+
+            glBindVertexArray(mVao);
+            glBindBuffer(GL_ARRAY_BUFFER, mVbo);
+            glBufferData(GL_ARRAY_BUFFER, sizeof(quadVertices), quadVertices, GL_DYNAMIC_DRAW);
+
+            // Position Attribute
+            glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 4 * sizeof(GLfloat), (void*)0);
+            glEnableVertexAttribArray(0);
+
+            // Texture Coordinate Attribute
+            glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 4 * sizeof(GLfloat), (void*)(2 * sizeof(GLfloat)));
+            glEnableVertexAttribArray(1);
+
+            glBindBuffer(GL_ARRAY_BUFFER, 0);
+            glBindVertexArray(0);
+
+            };
+        auto createTexture = [&]() {
+
+            glGenTextures(1, &mTexture);
+
+            glEnable(GL_TEXTURE_2D);
+            glBindTexture(GL_TEXTURE_2D, mTexture);
+            glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA8, mTextureWidth, mTextureHeight, 0, GL_RGBA, GL_UNSIGNED_INT_8_8_8_8_REV, nullptr);
+
+            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+
+            glBindTexture(GL_TEXTURE_2D, 0); // Unbind for safety
+            };
+
+        setupModernGL();
+        createQuad();
+        createTexture();
+
+        glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+
+        };
+
+    initGL();
+
+    mSelectedStripes = mStripes.begin();
+
+}
+void Animator::shutdown() {
+
+    auto shutdownGL = [&]() {
+
+        glDeleteTextures(1, &mTexture);
+
+        glDeleteBuffers(1, &mVbo); // Delete Vertex Buffer Object
+
+        glDeleteVertexArrays(1, &mVao); // Delete Vertex Array Object
+
+        glDeleteProgram(mShaderProgram);
+
+        SDL_GL_DestroyContext(mGLContext);
+
+        SDL_DestroyWindow(mWindow);
+        SDL_Quit();
+
+        };
+
+    shutdownGL();
+}
+
+void Animator::run(StepFunction&& step) {
+
+    mRunning = true;
+
+    nanoseconds lag(0), elapsedTime(0);
+    steady_clock::time_point nowTime, oldTime = steady_clock::now() - mLengthOfStep;
+    std::uint32_t tickCount = 0;
+
+    do {
+        nowTime = high_resolution_clock::now();
+        elapsedTime = nowTime - oldTime;
+        oldTime = nowTime;
+
+        lag += elapsedTime;
+        while (lag >= mLengthOfStep) {
+
+            step(mFloats);
+
+            FloatSpaceConvert::floatSpaceConvert(mFloats, mPixels, mColorizeMode);
+
+            render();
+
+            lag -= mLengthOfStep;
+
+            ++tickCount;
+        }
+
+        doEvents();
+
+    } while (mRunning);
+}
+
+void Animator::animateStatic() {
+
+    std::mt19937 random;
+    std::uniform_real_distribution<float> range(-1.0f, 1.0f);
+    std::vector<float> floats(getSize());
+
+    auto step = [&](auto floats) {
+
+        std::generate(std::execution::seq, floats.begin(), floats.end(), [&]() {
+            return range(random);
+            });
+        };
+
+    setup(floats);
+    run(step);
+}
+
+std::size_t Animator::getSize() { return mTextureWidth * mTextureHeight; }
+
---
+++ Animator/Animator.h
@@ -0,0 +1,70 @@
+#pragma once
+
+#include <SDL3/SDL.h>
+#include <GL/glew.h>
+#include <GL/wglew.h>
+
+#include <FloatSpaceConvert.h>
+
+#include <array>
+#include <system_error>
+#include <chrono>
+#include <functional>
+
+using namespace std::chrono;
+using namespace std::chrono_literals;
+
+class Animator {
+public:
+    struct Error : public std::system_error {
+
+        Error(std::errc code, const std::string& message);
+        static void sdlError();
+        static void glewError(auto error);
+        static void glCompilationError(auto shaderProgram);
+        void msgbox() const;
+    };
+
+    constexpr static std::size_t mWindowWidth = 1920, mWindowHeight = 1080;
+    constexpr static float mAspectRatio = float(mWindowWidth) / mWindowHeight;
+    constexpr static nanoseconds mLengthOfStep = nanoseconds(1s) / 7;
+
+private:
+
+    std::size_t mTextureWidth = 0, mTextureHeight = 0;
+
+    using PixelsView = std::span<std::uint32_t>;
+    std::vector<std::uint32_t> mPixels;
+    using FloatsView = std::span<float>;
+    FloatsView mFloats;
+
+    using ColorizeMode = FloatSpaceConvert::ColorizeMode;
+    ColorizeMode mColorizeMode = ColorizeMode::ROYGBIV;
+    using Stripes = std::array<const std::size_t, 5>;
+    Stripes mStripes = { 1,2,10,50,100 };
+    Stripes::iterator mSelectedStripes;
+    float mX = 0.0f, mY = 0.0f, mTranslateSpeed = 0.1f, mScale = 1.0f;
+
+    SDL_GLContext mGLContext = nullptr;
+    SDL_Window* mWindow = nullptr;
+    GLuint mTexture = 0, mShaderProgram = 0, mVao = 0, mVbo = 0;
+
+    bool mRunning = false;
+
+    void render();
+    void doEvents();
+
+public:
+    Animator(std::size_t width, std::size_t height);
+    ~Animator();
+
+    void setup(FloatsView floats);
+    void shutdown();
+
+    using StepFunction = std::function<void(FloatsView)>;
+    void run(StepFunction&& step);
+
+    void animateStatic();
+
+    std::size_t getSize();
+};
