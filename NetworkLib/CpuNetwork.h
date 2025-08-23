55964c47dc40055d6e51698294bf541e6a07596e
nicholaskomsa <nicholaskomsa@hotmail.com>
2025-08-23 12:51:27 -0400

    started work on TensorNew

--- NetworkLib/CpuNetwork.h
+++ NetworkLib/CpuNetwork.h
@@ -1,10 +1,51 @@
 #pragma once
 
+#include <vector>
+#include <mdspan>
 
 namespace NetworkLib {
 
+	using Floats = std::vector<float>;
+	using Extents1 = std::extents<size_t, std::dynamic_extent>;
+	using Extents2 = std::extents<size_t, std::dynamic_extent, std::dynamic_extent>;
+	using Extents3 = std::extents<size_t, std::dynamic_extent, std::dynamic_extent, std::dynamic_extent>;
+	using View1 = std::mdspan<float, Extents1>;
+	using View2 = std::mdspan<float, Extents2>;
+	using View3 = std::mdspan<float, Extents3>;
+
+	template<typename T>
+	concept View = std::is_same_v<T, View1> || std::is_same_v<T, View2> || std::is_same_v<T, View3>;
+
+	template<View ViewT>
+	class TensorNew {
+	public:
+
+		Floats mFloats;
+		ViewT mView;
+
+		template<typename... Dimensions>
+		void create(Dimensions&& ...dimensions) {
+
+			std::size_t size = 1;
+			(size *= ... *= dimensions);
+			mFloats.resize(size);
+
+			mView =  ViewT(mFloats.data(), dimensions...);
+		}
+
+		template<typename... Dimensions>
+		float& at(Dimensions&& ...dimensions) {
+			return mView[std::array{ dimensions... }];
+		}
+	};
+
+	using Tensor1 = TensorNew<View1>;
+	using Tensor2 = TensorNew<View2>;
+	using Tensor3 = TensorNew<View3>;
+
 	class CpuNetwork {
 	
+
 	public:
 
 	};
--- NetworkLibTest/main.cpp
+++ NetworkLibTest/main.cpp
@@ -3,6 +3,8 @@
 #include <Gpt2.h>
 #include <FloatSpaceConvert.h>
 
+#include <CpuNetwork.h>
+
 int main() {
 
 	using GPT2 = NetworkLib::GPT2;
@@ -15,7 +17,13 @@ int main() {
 
 	//diag.SGDTest();
 	//diag.serializeTest();
-	diag.simpleChat();
+	//diag.simpleChat();
+	
+	NetworkLib::Tensor2 t2;
+	t2.create(5,5);
+	t2.at(1,1) = 7;
+//
+	std::cout << t2.at(1,1);
 
 	std::puts("\nProgram Finished press enter to exit");
 	std::cin.get();
